e8cf5dcbfae2700ebfab4d94f1b7d3cbbc835c58
Santiago Montano <slincem@gmail.com>
2024-01-23 23:04:55 -0500

    initial commit

---
+++ src/main/java/guru/sfg/msscinventoryfailover/config/LocalDiscovery.java
@@ -0,0 +1,13 @@
+package guru.sfg.msscinventoryfailover.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@EnableDiscoveryClient
+@Profile("local-discovery")
+@Configuration
+public class LocalDiscovery {
+
+
+}
